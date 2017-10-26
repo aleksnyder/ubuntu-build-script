cf2caadaf2a8a61966cc05b899dc63f4f361387e
Alek Snyder <asnyder0816@gmail.com>
2017-10-25 20:54:24 -0400

    Added php packages. --acs

--- build.sh
+++ build.sh
@@ -1,80 +1,56 @@
 #!/bin/bash
 
 #
-echo
-echo
-echo
-echo
-echo
 echo "First things first, let's make sure we have the latest updates."
 echo "---------------------------------------------------------------"
 #
-apt-get update && apt-get -y upgrade
+apt-get update && apt-get upgrade -y
 #
 
 #
-echo
-echo
-echo
-echo
-echo
 echo "Now let's install git..."
 echo "---------------------------------------------------------------"
 #
 apt-get install -y git
 
 #
-echo
-echo
-echo
-echo
-echo
-echo "...and some Apache while we're at it..."
-echo "---------------------------------------------------------------"
-#
-apt-get install -y apache2
-
-#
-echo
-echo
-echo
-echo
-echo "..and some essentials."
+echo "...and some essentials..."
 echo "---------------------------------------------------------------"
 #
 apt-get install -y build-essential
 
 #
-echo
-echo
-echo
-echo
-echo
+echo "...and some Apache while we're at it."
+echo "---------------------------------------------------------------"
+#
+apt-get install -y apache2
+service apache2 start
+
+#
 echo "PHP time!"
 echo "---------------------------------------------------------------"
 #
-apt-get install -y php-curl php-gd php-dom php-cli
-apache2ctl restart
+apt-get install -y software-properties-common
+add-apt-repository -y ppa:ondrej/php
+apt-get update
+apt-get remove php7.0
+apt-get install -y --allow-unauthenticated php7.1 php7.1-fpm php7.1-mbstring php7.1-mysql php7.1-zip php7.1-gd php7.1-xml php7.1-curl php7.1-intl php7.1-json php7.1-mcrypt libapache2-mod-php7.1
+a2enmod actions
+service apache2 restart
 
 #
-echo
-echo
-echo
-echo
-echo
-echo "Now some Node.JS"
+echo "Now some NodeJS"
 echo "---------------------------------------------------------------"
 #
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-apt-get install nodejs
+apt-get install -y nodejs
 
 #
-echo
-echo
-echo
-echo
-echo
 echo "But what about our newer friend Composer?"
 echo "---------------------------------------------------------------"
 #
-curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+curl -o /tmp/composer-setup.php https://getcomposer.org/installer \
+&& curl -o /tmp/composer-setup.sig https://composer.github.io/installer.sig \
+&& php -r "if (hash('SHA384', file_get_contents('/tmp/composer-setup.php')) !== trim(file_get_contents('/tmp/composer-setup.sig'))) { unlink('/tmp/composer-setup.php'); echo 'Invalid installer' . PHP_EOL; exit(1); }" \
+&& php /tmp/composer-setup.php --no-ansi --install-dir=/usr/local/bin --filename=composer --snapshot \
+&& rm -f /tmp/composer-setup.*
