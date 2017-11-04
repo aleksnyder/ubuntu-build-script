192ab607ec10b38e8e42865a6e5282d3c13990ff
Alek Snyder <asnyder0816@gmail.com>
2017-11-03 21:19:26 -0400

    Fixed encoding.  Added Xdebug, commad line enchancements. --acs

--- build.sh
+++ build.sh
@@ -1,56 +1,67 @@
-#!/bin/bash
+#!/bin/sh
 
-#
-echo "First things first, let's make sure we have the latest updates."
-echo "---------------------------------------------------------------"
-#
-apt-get update && apt-get upgrade -y
-#
+################################################################
+# Bash script to install Apache, Git, Node, PHP 7, and Composer.
+################################################################
 
-#
-echo "Now let's install git..."
-echo "---------------------------------------------------------------"
-#
-apt-get install -y git
+#COLORS
+# Reset
+Color_Off='\033[0m'       # Text Reset
 
-#
-echo "...and some essentials..."
-echo "---------------------------------------------------------------"
-#
-apt-get install -y build-essential
+# Regular Colors
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
 
-#
-echo "...and some Apache while we're at it."
-echo "---------------------------------------------------------------"
-#
-apt-get install -y apache2
-service apache2 start
+# Update packages and Upgrade system
+echo "$Cyan \n Updating System.. $Color_Off"
+apt-get update -y && apt-get upgrade -y
 
-#
-echo "PHP time!"
-echo "---------------------------------------------------------------"
-#
-apt-get install -y software-properties-common
-add-apt-repository -y ppa:ondrej/php
-apt-get update
-apt-get remove php7.0
-apt-get install -y --allow-unauthenticated php7.1 php7.1-fpm php7.1-mbstring php7.1-mysql php7.1-zip php7.1-gd php7.1-xml php7.1-curl php7.1-intl php7.1-json php7.1-mcrypt libapache2-mod-php7.1
-a2enmod actions
+## Install Essentials
+echo "$Cyan \n Installing Apache2 & Essentials $Color_Off"
+apt-get install apache2 nano build-essential -y
+
+## Install Git
+echo "$Green \n Installing Git $Color_Off"
+apt-get install git -y
+
+## Install PHP
+echo "$Cyan \n Installing PHP & Requirements $Color_Off"
+add-apt-get-repository -y ppa:ondrej/php
+apt-get update && apt-get install php7.1 php7.1-fpm php7.1-mbstring php7.1-mysql php7.1-zip php7.1-gd php7.1-xml php7.1-curl php7.1-intl php7.1-json php7.1-mcrypt libapache2-mod-php7.1 libapache2-mod-fastcgi php-xdebug -y
+
+# Xdebug
+echo "zend_extension=/usr/lib/php/20160303/xdebug.so" >> /etc/php/7.1/apache2/php.ini
+echo "xdebug.remote_enable=1" >> /etc/php/7.1/apache2/php.ini
+echo "xdebug.remote_host=127.0.0.1" >> /etc/php/7.1/apache2/php.ini
+echo "xdebug.remote_port = 9000" >> /etc/php/7.1/apache2/php.ini
+
+# Enabling Mod Rewrite
+echo "$Cyan \n Enabling Modules $Color_Off"
+a2enmod rewrite
+a2enmod fastcgi
+
+# Restart Apache
+echo "$Cyan \n Restarting Apache $Color_Off"
 service apache2 restart
 
-#
-echo "Now some NodeJS"
-echo "---------------------------------------------------------------"
-#
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-apt-get install -y nodejs
+## Install NodeJS
+echo "$Cyan \n Installing Node.js $Color_Off"
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+apt-get update && apt-get install nodejs -y
+apt-get install build-essential -y
 
-#
-echo "But what about our newer friend Composer?"
-echo "---------------------------------------------------------------"
-#
-curl -o /tmp/composer-setup.php https://getcomposer.org/installer \
-&& curl -o /tmp/composer-setup.sig https://composer.github.io/installer.sig \
-&& php -r "if (hash('SHA384', file_get_contents('/tmp/composer-setup.php')) !== trim(file_get_contents('/tmp/composer-setup.sig'))) { unlink('/tmp/composer-setup.php'); echo 'Invalid installer' . PHP_EOL; exit(1); }" \
-&& php /tmp/composer-setup.php --no-ansi --install-dir=/usr/local/bin --filename=composer --snapshot \
-&& rm -f /tmp/composer-setup.*
+## Install Composer
+echo "$Cyan \n Installing Composer $Color_Off"
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+
+## Add git branch to PS1
+echo "\n# Add Git branch to command line" >> ~/.bashrc
+echo "parse_git_branch() {" >> ~/.bashrc
+echo "    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'" >> ~/.bashrc
+echo "}" >> ~/.bashrc
+
+PS1="\u@\h \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
+echo "export PS1=$PS1" >> ~/.bashrc
